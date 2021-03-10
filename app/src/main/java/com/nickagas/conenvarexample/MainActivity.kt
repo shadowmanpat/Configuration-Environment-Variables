b44cae1ef889f345b404ddde094942a6137701e7
nagas <n.agas@exus.co.uk>
2021-03-10 22:18:20 +0200

    added module as local dependency

--- app/src/main/java/com/nickagas/conenvarexample/MainActivity.kt
+++ app/src/main/java/com/nickagas/conenvarexample/MainActivity.kt
@@ -5,10 +5,12 @@ import android.os.Bundle
 import android.util.Log.d
 import com.nickagas.conenvar.LogDebug
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         LogDebug().d("hello")
+
     }
 }
