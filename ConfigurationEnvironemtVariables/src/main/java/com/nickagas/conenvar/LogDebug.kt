a199dc36de477ba393a197ada06c92c95c5eea44
nagas <n.agas@exus.co.uk>
2021-03-10 22:32:10 +0200

    notification in progress

--- ConfigurationEnvironemtVariables/src/main/java/com/nickagas/conenvar/LogDebug.kt
+++ ConfigurationEnvironemtVariables/src/main/java/com/nickagas/conenvar/LogDebug.kt
@@ -1,6 +1,8 @@
 package com.nickagas.conenvar
 
+import android.content.Context
 import android.util.Log
+import androidx.core.app.NotificationCompat
 
 val myname = "NIKOS"
 class LogDebug {
@@ -19,5 +21,13 @@ object AwesomeToast {
 
     public fun d(message: String){
         Log.d(TAG, message)
+    }
+
+    fun start( context : Context){
+        var builder = NotificationCompat.Builder(context, TAG)
+//            .setSmallIcon(R.drawable.notification_icon)
+            .setContentTitle("My Awesome Title")
+            .setContentText("textContent")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
     }
 }
